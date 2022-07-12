933aa5336778135d418159a9a5f7431d74ecfa89
unknown <abdulkaderjilani120@gmail.com>
2022-07-12 23:30:24 +0600

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Page/Navbar/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from './Page/Home/Home';
+import Login from './Page/Login/Login';
+import Menu from './Page/Menu/Menu';
+import Contact from './Page/Contact/Contact';
+import Rservation from './Page/Reservation/Rservation';
+import NotFOundPage from './Page/NotFoundPage/NotFOundPage';
 
 function App() {
   return (
     <div className="">
      <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/home' element={<Home/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/menu' element={<Menu/>} />
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/reservation' element={<Rservation/>} />
+      <Route path='*' element={<NotFOundPage/>} />
+     </Routes>
     </div>
   );
 }
---
+++ src/Page/Reservation/Rservation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rservation = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Rservation;
