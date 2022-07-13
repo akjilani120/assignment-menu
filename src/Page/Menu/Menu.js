c1636650c7bbb685009ee942d62d738c651f2811
unknown <abdulkaderjilani120@gmail.com>
2022-07-13 11:49:06 +0600

    added menu banner

--- src/Page/Menu/Menu.js
+++ src/Page/Menu/Menu.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {  useQuery } from 'react-query'
 import IsLoading from '../IsLoading/IsLoading';
+import ProductShow from './ProductShow';
+import background from '../img/picca-1.jfif'
+import './Menu.css'
 const Menu = () => {
     const { isLoading, error, data } = useQuery('orderData', () =>
     fetch('https://myqa.fleksa.com/pyapi/26/menu').then(res =>
@@ -8,16 +11,26 @@ const Menu = () => {
     )
   )
   if (isLoading) return <IsLoading></IsLoading>
-  if (error) return  error.message
-  console.log("order data" , data)
+
+ const orders = data.categories
+ console.log("offers data" , data.offers)
+ console.log("parts data" ,data.parts)
+ console.log("sides data" ,data.sides)
     return (
         <div>
             <div className="row">
+                <div className="col-12 banner" style={{ backgroundImage: `url(${background})` }} >
+                  <div className="banner-overflow">
+                  
+                  </div>
+                </div>
+            </div>
+            <div className="row">
                 <div className="col-3">Categorious</div>
                 <div className="col-5">
-                    {
-                        data.map(order => order)
-                    }
+                   {
+                    orders.map(order => <ProductShow order ={order} key={order.id}></ProductShow>)
+                   }
                 </div>
                 <div className="col-4">Add to card</div>
                 
